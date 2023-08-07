7eb94a5e8e3268f6961e06da80a8f361cbf0657c
unknown <sdhakad011@gmail.com>
2023-08-07 18:43:57 +0530

    home page updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,16 +4,24 @@ import pic from "../Assets/pic.png";
 const Home = () => {
   return (
     <div className="flex flex-row text-white h-full w-full mt-3">
-      <div className="flex flex-col justify-center items-center w-1/2 pl-3">
+      <div className="flex flex-col justify-center md:ml-10 lg:ml-24 w-1/2 pl-3">
+        <h1 className="text-4xl text-green-400 font-bold mb-5">Sudama Dhakad</h1>
+
         <TypeAnimation
           sequence={[
-            `Hi, I'm Sudama Dhakad. I'm a Full Stack Developer. My goal is to keep pushing the boundaries of what's possible in web development.
-            I'm constantly seeking new challenges and projects to expand my skills and create meaningful experiences for users. 
-            Thank you for visiting my portfolio! I hope you find my projects and creations inspiring. Let's connect and explore the possibilities of working together on exciting projects.`,
+            "I'm Student.",
+            1000,
+            "I'm Competitive Programmer.",
+            1000,
+            "I'm Full Stack Developer.",
+            1000,
+            "I'm Quick Learner.",
+            1000,
           ]}
+          wrapper="strong"
           speed={65}
-          style={{ whiteSpace: "pre-line", fontSize: "24px" }}
-          deletionSpeed={0}
+          style={{ whiteSpace: "pre-line", fontSize: "24px", color: "#f0f" }}
+          repeat={Infinity}
         />
       </div>
       <div className="flex flex-col  items-center w-1/2 h-fit">
